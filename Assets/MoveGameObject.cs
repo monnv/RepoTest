b3ed654e27527069fd686ac19729992ba813dcff
monica nguyen-vo <monnvart@gmail.com>
2020-07-13 19:21:32 -0500

    initial repo set up

---
+++ Assets/MoveGameObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MoveGameObject : MonoBehaviour
+{
+    // The speed that the GameObject will move
+    private float _speed = 10;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += (Vector3.forward * _speed) * Time.deltaTime;
+        }
+        // Move the GameObject up this frame
+
+
+    }
+
+}
